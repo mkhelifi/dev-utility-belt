eb7e0f34bed5a8260ca83be8f0c07e4f6e9731b7
Hildeberto Mendonca <me@hildeberto.com>
2015-02-16 00:14:46 +0100

    Added documentation to the property-file functions.

--- src/javaee_utility_belt/property-file.clj
+++ src/javaee_utility_belt/property-file.clj
@@ -2,34 +2,50 @@
   (:require [clojure.string :refer :all]))
 
 (defn load-properties [file]
-  (filterv (fn [line] (not (= 0 (.indexOf line "#"))))
+  "Opens a property file, loads its content and
+  returns a lazy sequence of properties."
+  (filterv (fn [line] (not= 0 (.indexOf line "#")))
   (filterv (fn [line] (not (blank? line)))
     (split-lines
       (slurp file)))))
 
 (defn save-properties [filename properties]
+  "Writes down to a text file a collection of properties."
   (spit filename (str "# List in alphabetic order\n" (join "\n" properties))))
 
 (defn merge-properties [file1 file2]
-  (loop [props1 (load-properties file1) props2 (load-properties file2) merged-properties []]
+  "Merges the properties of two property files and
+  returns a collection of the merged result."
+  (loop [props1 (load-properties file1)
+         props2 (load-properties file2)
+         merged-properties []]
     (if (empty? props1)
       (concat merged-properties props2)
       (if (empty? props2)
         (concat merged-properties props1)
         (let [c (compare (first props1) (first props2))]
           (if (= c 0)
-            (recur (rest props1) (rest props2) (conj merged-properties (first props1)))
+            (recur (rest props1)
+                   (rest props2)
+                   (conj merged-properties (first props1)))
             (if (< c 0)
-              (recur (rest props1) props2 (conj merged-properties (first props1)))
-              (recur props1 (rest props2) (conj merged-properties (first props2))))))))))
+              (recur (rest props1)
+                     props2
+                     (conj merged-properties (first props1)))
+              (recur props1
+                     (rest props2)
+                     (conj merged-properties (first props2))))))))))
 
 (defn group-by-alphabet [properties]
+  "Groups properties alphabetically for better manageability."
   (let [sorted-properties (sort properties)]
     (loop [letter "" sorted-properties sorted-properties grouped-properties []]
       (if (= (count sorted-properties) 0)
         grouped-properties
         (if (= letter (subs (first sorted-properties) 0 1))
-          (recur letter (rest sorted-properties) (conj grouped-properties (first sorted-properties)))
-          (recur (subs (first sorted-properties) 0 1) sorted-properties (conj grouped-properties (str "\n# " (subs (first sorted-properties) 0 1)))))))))
-
-
+          (recur letter
+                 (rest sorted-properties)
+                 (conj grouped-properties (first sorted-properties)))
+          (recur (subs (first sorted-properties) 0 1)
+                 sorted-properties
+                 (conj grouped-properties (str "\n# " (subs (first sorted-properties) 0 1)))))))))
