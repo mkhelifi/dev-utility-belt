bd251e7dc034b2c24e3ad737318e9a4047bdae0e
Hildeberto Mendonca <me@hildeberto.com>
2015-03-11 00:28:28 +0100

    Issue #4: the function log-records reads a log file and return a vector of records.

--- src/javaee_utility_belt/log_file.clj
+++ src/javaee_utility_belt/log_file.clj
@@ -1,21 +1,20 @@
 (ns javaee-utility-belt.log-file
   (:require [clojure.string :refer (blank? join split split-lines)]))
 
-(loop [lines (split-lines (slurp "test/logs/server.log"))
-       open false
-       record nil
-       records []]
-  (if (empty? lines)
-    records
-    (let [line (first lines)]
-      (let [level (re-find #"ERROR|INFO|SEVERE|WARNING" line)]
-        (case level
-          "ERROR"   (recur (rest lines) true  line records)
-          "INFO"    (recur (rest lines) false nil  (conj records line))
-          "SEVERE"  (recur (rest lines) true  line records)
-          "WARNING" (recur (rest lines) true  line records)
-          (recur (rest lines) true record records))))))
-
-
+(defn log-records [log-file]
+  (loop [lines (split-lines (slurp log-file))
+         record nil
+         records []]
+    (if (empty? lines)
+      (if (nil? record) records (conj records record))
+      (let [line (first lines)]
+        (let [level (re-find #"DEBUG|ERROR|INFO|SEVERE|WARNING" line)]
+          (case level
+            "DEBUG"   (recur (rest lines) line                   (if (nil? record) records (conj records record)))
+            "ERROR"   (recur (rest lines) line                   (if (nil? record) records (conj records record)))
+            "INFO"    (recur (rest lines) line                   (if (nil? record) records (conj records record)))
+            "SEVERE"  (recur (rest lines) line                   (if (nil? record) records (conj records record)))
+            "WARNING" (recur (rest lines) line                   (if (nil? record) records (conj records record)))
+                      (recur (rest lines) (str record "\n" line) records)))))))
 
 (map #(re-find #"ERROR|INFO|SEVERE|WARNING" %) (split-lines (slurp "test/logs/server.log")))
