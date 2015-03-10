c52363f3aa0d707ab813de5643c6abd2c5f7692f
Hildeberto Mendonca <me@hildeberto.com>
2015-03-10 13:33:13 +0100

    Further evolutions in the server.log reader.

--- src/javaee_utility_belt/core.clj
+++ src/javaee_utility_belt/core.clj
@@ -1,8 +1,3 @@
 (ns javaee-utility-belt.core
   (:require [javaee-utility-belt.property-file :refer :all])
   (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
--- src/javaee_utility_belt/log_file.clj
+++ src/javaee_utility_belt/log_file.clj
@@ -1,17 +1,21 @@
 (ns javaee-utility-belt.log-file
   (:require [clojure.string :refer (blank? join split split-lines)]))
 
-(loop [lines (split-lines (slurp "test\\logs\\server.log"))
+(loop [lines (split-lines (slurp "test/logs/server.log"))
        open false
+       record nil
        records []]
   (if (empty? lines)
     records
-    (let [level (re-find #"ERROR|INFO|SEVERE|WARNING" (first lines))]
-      (case level
-        "ERROR"   ()
-        "INFO"    ()
-        "SEVERE"  ()
-        "WARNING" ()
-        ()))))
+    (let [line (first lines)]
+      (let [level (re-find #"ERROR|INFO|SEVERE|WARNING" line)]
+        (case level
+          "ERROR"   (recur (rest lines) true  line records)
+          "INFO"    (recur (rest lines) false nil  (conj records line))
+          "SEVERE"  (recur (rest lines) true  line records)
+          "WARNING" (recur (rest lines) true  line records)
+          (recur (rest lines) true record records))))))
 
-(map #(re-find #"ERROR|INFO|SEVERE|WARNING" %) (split-lines (slurp "test\\logs\\server.log")))
+
+
+(map #(re-find #"ERROR|INFO|SEVERE|WARNING" %) (split-lines (slurp "test/logs/server.log")))
