a9368d16cb32e0d777eac6129c8fe5dac9cc16e8
Hildeberto Mendonca <me@hildeberto.com>
2015-03-11 10:30:01 +0100

    Issue #4: Optimization of the function log-records.

--- src/javaee_utility_belt/log_file.clj
+++ src/javaee_utility_belt/log_file.clj
@@ -1,20 +1,19 @@
 (ns javaee-utility-belt.log-file
   (:require [clojure.string :refer (blank? join split split-lines)]))
 
+(defn- conj-record [records record]
+  (if (nil? record) records (conj records record)))
+
 (defn log-records [log-file]
   (loop [lines (split-lines (slurp log-file))
          record nil
          records []]
     (if (empty? lines)
-      (if (nil? record) records (conj records record))
+      (conj-record records record)
       (let [line (first lines)]
         (let [level (re-find #"DEBUG|ERROR|INFO|SEVERE|WARNING" line)]
-          (case level
-            "DEBUG"   (recur (rest lines) line                   (if (nil? record) records (conj records record)))
-            "ERROR"   (recur (rest lines) line                   (if (nil? record) records (conj records record)))
-            "INFO"    (recur (rest lines) line                   (if (nil? record) records (conj records record)))
-            "SEVERE"  (recur (rest lines) line                   (if (nil? record) records (conj records record)))
-            "WARNING" (recur (rest lines) line                   (if (nil? record) records (conj records record)))
-                      (recur (rest lines) (str record "\n" line) records)))))))
+          (if (nil? level)
+            (recur (rest lines) (str record "\n" line) records)
+            (recur (rest lines) line                   (conj-record records record))))))))
 
-(map #(re-find #"ERROR|INFO|SEVERE|WARNING" %) (split-lines (slurp "test/logs/server.log")))
+(nth (log-records "test/logs/server.log") 13)
