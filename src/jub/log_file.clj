b4a54b5c9f4ec82c436b474168d0a2cebe862d5f
Hildeberto Mendonca <me@hildeberto.com>
2015-03-12 14:30:00 +0100

    Issue #4: Added documentation to the functions and a new function filter-by-level.

--- src/jub/log_file.clj
+++ src/jub/log_file.clj
@@ -2,12 +2,9 @@
   (:require [clojure.string :refer (blank? join split split-lines)]))
 
 (defn enclosed-within [sentence closure]
-  (let [openning-closure (case closure :parentheses  \(
-                                       :brackets     \[
-                                       :curly-braces \{)
-        closing-closure (case closure :parentheses   \)
-                                       :brackets     \]
-                                       :curly-braces \})]
+  "Returns the content inside of parentheses, brackets or braces found within a sentence."
+  (let [openning-closure (case closure :parentheses \( :brackets \[ :curly-braces \{)
+        closing-closure (case closure  :parentheses \) :brackets \] :curly-braces \})]
     (let [i (.indexOf sentence (str openning-closure))]
       (if (< i 0)
         nil
@@ -26,6 +23,7 @@
                                           (= parity 1)) "" pos-char))))))))))
 
 (defn decompose-log-record [record]
+  "Analyses and decomposes log record's parts into a map."
   (let [mapped-record {}]
     (assoc mapped-record :message   (.substring record (+ (.indexOf record ")") 2))
                          :thread    (enclosed-within record :parentheses)
@@ -34,11 +32,13 @@
                          :timestamp (re-find #"\d\d:\d\d:\d\d,\d\d\d" record))))
 
 (defn conj-record [records record]
+  "Adds a record in a collection of records but rejecting nil values."
   (if (nil? record)
     records
     (conj records (decompose-log-record record))))
 
 (defn log-records [log-file]
+  "Takes a log file and returns a structured representation of it in a vector of maps."
   (loop [lines (split-lines (slurp log-file))
          record nil
          records []]
@@ -54,3 +54,6 @@
                    line
                    (conj-record records record))))))))
 
+(defn filter-by-level [log-file level]
+  "Goes through the log records and returns the ones with the informed log level."
+  (filter #(= level (get % :level)) (log-records log-file)))
