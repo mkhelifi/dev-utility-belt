54e88ad8ad8c81522e465d5ef4a2cc4a834bd36f
Hildeberto Mendonca <me@hildeberto.com>
2015-03-22 19:09:18 +0100

    Issue #7 Added database migration and connection pooling.

--- src/jub/datasource.clj
+++ src/jub/datasource.clj
@@ -1,18 +1,31 @@
 (ns jub.datasource
   (:require [clojure.java.jdbc    :as jdbc]
             [joplin.core          :as joplin]
-            [joplin.sql.database]))
+            [joplin.jdbc.database])
+  (:import com.zaxxer.hikari.HikariDataSource))
 
-(def mysql-db {:subprotocol "mysql"
-               :subname     "//localhost:3306/jub"
-               :user        "jub_user"
-               :password    "senhas"})
+(def mysql-db-spec {:classname   "com.mysql.jdbc.Driver"
+                    :subprotocol "mysql"
+                    :subname     "//localhost:3306/jub"
+                    :user        "jub_user"
+                    :password    "senhas"})
 
 (def joplin-target {:db {:type :sql
-                         :url (str "jdbc:mysql:" (get mysql-db :subname)
-                                   "?user="      (get mysql-db :user)
-                                   "&password="  (get mysql-db :password))}
+                         :url (str "jdbc:mysql:" (:subname mysql-db-spec)
+                                   "?user="      (:user mysql-db-spec)
+                                   "&password="  (:password mysql-db-spec))}
                     :migrator "resources/migrators/sql"})
 
 (defn migrate-mysql-db []
   (joplin/migrate-db joplin-target))
+
+(defn connection-pool []
+  (let [hikari (doto (HikariDataSource.)
+                 (.setJdbcUrl (str "jdbc:" (:subprotocol mysql-db-spec) ":" (:subname mysql-db-spec)))
+                 (.setUsername (:user mysql-db-spec))
+                 (.setPassword (:password mysql-db-spec)))]
+    {:datasource hikari}))
+
+(def pooled-db (delay (connection-pool)))
+
+(defn db-connection [] @pooled-db)
