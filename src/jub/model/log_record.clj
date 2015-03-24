f0a4a3b85cd2a38838b1188d08b3600f9bad3deb
Hildeberto Mendonca <me@hildeberto.com>
2015-03-24 17:18:33 +0100

    Issue #7: select-all and select-by-interval.

--- src/jub/model/log_record.clj
+++ src/jub/model/log_record.clj
@@ -1,6 +1,21 @@
 (ns jub.model.log-record
-  (:require [clojure.java.jdbc :as jdbc]
-            [jub.datasource :refer (db-connection) :as datasource]))
+  (:require [clojure.java.jdbc :as    jdbc]
+            [honeysql.core     :as    sql]
+            [honeysql.helpers  :refer :all]
+            [jub.datasource    :refer (db-connection)]))
 
 (defn insert-record []
-  (jdbc/with-db-connection (datasource/db-connection) ))
+  (jdbc/with-db-connection
+    (datasource/db-connection)
+    (insert-into)))
+
+(defn find-all []
+  (let [sql {:select :*
+             :from   :log_record}]))
+
+(defn find-by-interval [begin end]
+  (let [sql {:select :*
+             :from   :log_record
+             :where  [:and
+                      [:>= :l.instant begin]
+                      [:<= :l.instant end]]}]))
