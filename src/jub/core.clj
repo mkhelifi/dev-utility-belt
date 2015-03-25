fe9db390fe0995de8e3eae7825c34ea0ddcee5a2
Hildeberto Mendonca <me@hildeberto.com>
2015-03-25 11:53:42 +0100

    Issue #7 Updated references in core.clj

--- src/jub/core.clj
+++ src/jub/core.clj
@@ -1,7 +1,8 @@
 (ns jub.core
-  (:require [jub.log-file      :refer :all]
-            [jub.property-file :refer (organize-alphabetically merge-properties replace-properties)]
-            [jub.datasource    :refer (migrate-mysql-db)])
+  (:require [jub.log-file        :refer :all]
+            [jub.property-file   :refer (organize-alphabetically merge-properties replace-properties)]
+	    [jub.model.log-file] :refer :all]
+            [jub.datasource      :refer (migrate-mysql-db)])
   (:gen-class))
 
 (migrate-mysql-db)
