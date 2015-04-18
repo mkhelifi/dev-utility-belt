48eca71c92b007db4b19e1c99dfe2c0774bc7705
Hildeberto Mendonca <me@hildeberto.com>
2015-04-18 12:28:47 +0200

    Updated the project.clj file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject javaee-utility-belt "0.1.0-SNAPSHOT"
-  :description "This project aims to fullfil the gap to effective productivity on the development of Java EE projects."
-  :url "https://github.com/htmfilho/javaee-utility-belt"
+(defproject dev-utility-belt "0.1.0-SNAPSHOT"
+  :description "This project offers a set of functionalities to make the life of programmers easier."
+  :url "https://github.com/EsmerilProgramming/dev-utility-belt"
   :license {:name "Apache License V2"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure        "1.6.0" ] ; Latest stable vertion of Clojure.
@@ -11,6 +11,6 @@
                  [joplin.core                "0.2.9" ] ; Database migration
                  [joplin.jdbc                "0.2.9" ] ; Database migration for relational dbs
                  [com.zaxxer/HikariCP        "2.3.5" ]]; Database connection pooling
-  :main ^:skip-aot jub.core
+  :main ^:skip-aot dub.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
