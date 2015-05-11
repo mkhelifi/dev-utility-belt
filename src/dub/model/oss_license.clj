d4da8924129c62b0082c00bab78c9c267c6d3d40
Hildeberto Mendonca <me@hildeberto.com>
2015-05-11 22:43:49 +0200

    Issue #1: Planned the composition of functions. From now on, it is all about coding!

---
+++ src/dub/model/oss_license.clj
@@ -0,0 +1,35 @@
+(ns dub.model.oss-license
+  (:require [clojure.string            :refer (blank? join split split-lines)]
+            [dub.utils                 :as utils]))
+
+(defn project-files [project-path]
+  "Returns a sequence of absolute paths to all files in the project."
+  project-path)
+
+(defn filter-extensions [files extensions]
+  "Takes a sequence of files and return only the ones with the informed extensions."
+  extensions)
+
+(defn append-license [license files]
+  "Takes each file from the sequence and adds the license to the top of the file."
+  license)
+
+(defn detach-license [files]
+  "Takes each file from the sequence and detach the license from the top of the file."
+  files)
+
+(defn set-license [license project-path & extensions]
+  "Define the license of an open source software. It adds the main license file
+   to the root of the project and modify the header of all source files adding
+   the respective license short note."
+  (append-license license
+                  (filter-extensions (project-files project-path)
+                                     extensions)))
+
+(defn remove-license [project-path & extensions]
+  "Remove the license of an open source software. Useful to replace the current license by
+  another license or fixing something that went wrong."
+  (detach-license (filter-extensions (project-files project-path)
+                                     extensions)))
+
+(set-license :apache-v2 "/home/clojure/project/test" :java :properties :html)
